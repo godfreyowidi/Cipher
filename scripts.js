1dc0e2e13b36fd101f2ed9238c3ea8c9073e1b9f
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-03 02:46:24 +0300

    add initial functions.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let sentence = prompt("Enter a Sentence: ");
+alert(sentence);
+
+function firstLast() {
+  let capitalize = (sentence.charAt(0) + sentence.charAt(sentence.length-1));
+  return results = (capitalize.toUpperCase());
+  
+}
+  
+alert(firstLast());
+
+function reverseString() {
+  let reverse = (sentence.charAt(sentence.length-1) + sentence.charAt(0));
+  return reverse.toUpperCase();
+}
+
+alert(reverseString());
+
+function conateSentence(firstLast(reverseString() {
+});
